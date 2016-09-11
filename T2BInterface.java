8b06ce32f6381e3eb54ea5295b2586e910a9031a
alexhrao <alexhrao@gmail.com>
2016-09-10 22:31:15 -0400

    Adding interface

---
+++ T2BInterface.java
@@ -0,0 +1,5 @@
+package com.chickasaw.text2braille;
+
+public interface T2BInterface {
+
+}
---
+++ Text2Braille.java
@@ -0,0 +1,7 @@
+package com.chickasaw.text2braille;
+
+public class Text2Braille {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
