0d6343330932cb3fa401a39907a8d485c9ac83b3
alexhrao <alexhrao@gmail.com>
2016-09-11 17:35:55 -0400

    Adding support for abbreviations

---
+++ Pair.java
@@ -0,0 +1,20 @@
+public class Pair implements Comparable<Pair> {
+    public final int index;
+    public final int value;
+
+    public Pair(int index, int value) {
+        this.index = index;
+        this.value = value;
+    }
+    public int getVal() {
+        return this.value;
+    }
+    public int getInd() {
+        return this.index;
+    }
+
+    @Override
+    public int compareTo(Pair other) {
+        return -1 * Integer.valueOf(this.value).compareTo(other.value);
+    }
+}
--- Text2Braille.java
+++ Text2Braille.java
@@ -1,35 +1,43 @@
 import java.io.*;
+import java.util.Arrays;
 public class Text2Braille {
-    private static String[] strChars;
-    private static String[] strCodes;
-    private static int intChars;
-    private static int intBrailleChars;
+    private String[] strChars;
+    private String[] strCodes;
+    private Pair[] intChrLen;
+    private int intChars;
+    private int intBrailleChars;
 
     public Text2Braille(String strInput) {
         intChars = strInput.length();
-        Text2Braille.initialize();
-        
-
+        this.initialize("default");
     }
-
+    public Text2Braille(String strInput, String encoding) {
+        intChars = strInput.length();
+        this.initialize(encoding);
+    }
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Please provide more arguments!");
         } else {
-            Text2Braille.initialize();
-            String[] code = Text2Braille.text2Bin(args[0]);
-            char[][] dispCode = Text2Braille.bin2Braille(code);
-            for (int count = 0; count < dispCode.length; count++) {
-                for (int count2 = 0; count2 < dispCode[count].length; count2++) {
-                    System.out.print(dispCode[count][count2]);
+            for (String str:args) {
+                Text2Braille ttb = new Text2Braille(str);
+                String[] code = ttb.text2Bin(args[0]);
+                char[][] dispCode = ttb.bin2Braille(code);
+                for (int count = 0; count < dispCode.length; count++) {
+                    for (int count2 = 0; count2 < dispCode[count].length; count2++) {
+                        System.out.print(dispCode[count][count2]);
+                    }
+                    System.out.print("\r\n");
                 }
-                System.out.print("\r\n");
             }
         }
     }
-    public static String[] text2Bin(String input) {
+    public String[] text2Bin(String input) {
         String[] output = new String[input.toCharArray().length];
         int intCount = 0;
+        for (int count = 0; count < strChars.length; count++) {
+            
+        }
         for (char c:input.toCharArray()) {
             for (int count = 0; count < strChars.length; count++) {
                 if (String.valueOf(c).toUpperCase().equals(strChars[count])) {
@@ -40,7 +48,7 @@ public class Text2Braille {
         }
         return output;
     }
-    public static char[][] bin2Braille(String[] bin) {
+    public char[][] bin2Braille(String[] bin) {
         char[][] dispCode = new char[3][bin.length * 2];
         int intCount = 0;
         for (String str:bin) {
@@ -54,10 +62,10 @@ public class Text2Braille {
         }
         return dispCode;
     }
-    public static void initialize() {
+    public void initialize(String encoding) {
         String line = null;
         int intLines = 0;
-        try (BufferedReader inputStream = new BufferedReader(new FileReader("table.txt"))) {
+        try (BufferedReader inputStream = new BufferedReader(new FileReader(encoding + ".txt"))) {
             while ((line = inputStream.readLine()) != null) {
                 intLines++;
             }
@@ -65,19 +73,35 @@ public class Text2Braille {
             System.out.println("SHIT!");
             System.exit(1);
         }
-        try (BufferedReader inputStream = new BufferedReader(new FileReader("table.txt"))) {
+        try (BufferedReader inputStream = new BufferedReader(new FileReader(encoding + ".txt"))) {
             line = null;
             strChars = new String[intLines];
             strCodes = new String[intLines];
+            intChrLen = new Pair[intLines];
             int intCount = 0;
             while ((line = inputStream.readLine()) != null) {
                 strChars[intCount] = line.substring(0, line.indexOf(':', 1));
+                if (!strChars[intCount].substring(0, 1).equals("%")) {
+                    intChrLen[intCount] = new Pair(intCount, 0);
+                } else {
+                    intChrLen[intCount] = new Pair(intCount, strChars[intCount].length());
+                }
                 strCodes[intCount] = line.substring(line.indexOf(':', 1) + 1, line.indexOf(';', 1));
                 intCount++;
             }
         } catch (IOException x) {
             System.out.println("SHIT!");
             System.exit(1);
+        }
+        Arrays.sort(intChrLen);
+        String[] strCharsCopy = strChars;
+        String[] strCodesCopy = strCodes;
+        int index = 0;
+        int intCount = 0;
+        for (Pair ind:intChrLen) {
+            index = ind.getInd();
+            strChars[intCount] = strCharsCopy[index];
+            strCodes[intCount] = strCodesCopy[index];
         }
     }
 }
